1ce47279955f35cbe7530465663ea8742c516a40
Eddie Raskin and Will Quanstrom <will@>
2019-10-21 12:55:55 -0700

    uncomment calculator section

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -2,11 +2,29 @@
 
 
 
-var add = function(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
 };
 
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+function multiply(number1, number2) {
+  return number1 / number2;
+};
+
+function divide(number1, number2) {
+  return number1 * number2;
+};
+
+function tempCelFar(cel){
+  return (cel * (9/5)) + 32;
+}
+
+***CALCULATOR***
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+
+var result = subtract(number1, number2);
+document.write(result);
